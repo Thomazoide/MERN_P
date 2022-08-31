429a60ab87e652bee7501e6a71d8b3cc36f77782
Thomazoide <t.cannobbioabarca@uandresbello.edu>
2022-08-31 14:11:01 -0400

    modelo usuario

---
+++ models/usuario.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const usuarioSchema = new mongoose.Schema({
+    email: String,
+    pass: String
+});
+module.exports = mongoose.model("usuario", usuarioSchema);
